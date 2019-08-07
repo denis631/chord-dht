3dc97e0cefe3c51d0c3cd7ece397e0a4fca7249d
Denis Grebennicov <denisgrebennicov@gmail.com>
2019-08-07 20:26:00 +0200

    - refactoring. better naming and splitting into functions

--- modules/messages/src/main/scala/Messages.scala
+++ modules/messages/src/main/scala/Messages.scala
@@ -3,13 +3,13 @@ package messages;
 import akka.http.scaladsl.marshallers.sprayjson.SprayJsonSupport
 import spray.json.{DefaultJsonProtocol, RootJsonFormat}
 
-sealed trait PeerStatus {
+sealed trait PeerState {
   val `type`: String
   val nodeId: Long
 }
 
-case class PeerDied(nodeId: Long, `type`: String = "NodeDeleted") extends PeerStatus
-case class PeerUpdate(nodeId: Long, successorId: Long, `type`: String = "SuccessorUpdated") extends PeerStatus
+case class PeerDied(nodeId: Long, `type`: String = "NodeDeleted") extends PeerState
+case class PeerUpdate(nodeId: Long, successorId: Long, `type`: String = "SuccessorUpdated") extends PeerState
 
 trait MessagesJSONFormatting extends SprayJsonSupport with DefaultJsonProtocol {
   implicit val peerConnectionsFormat: RootJsonFormat[PeerUpdate] = jsonFormat3(PeerUpdate)
